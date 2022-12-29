5312a35bbb91df176aafcd151d5a339e7445b02e
teach <morus95@mail.ru>
2022-12-29 12:15:23 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+
+        System.out.println();
+
+        System.out.println("Задача 2");
+        System.out.println();
+
+        System.out.println("Задача 3");
+        System.out.println();
+
+        System.out.println("Задача 4");
+        System.out.println();
+
+        System.out.println("Задача 5");
+        System.out.println();
+
+        System.out.println("Задача 6");
+        System.out.println();
+
+        System.out.println("Задача 7");
+    }
+}
